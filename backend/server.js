6e2f5f20adb441ff7b32e55265707ec18de37fc5
Tomari <tomari7878@gmail.com>
2024-12-15 16:59:31 -0600

    started node.js setup

--- backend/server.js
+++ backend/server.js
@@ -1,2 +1,12 @@
 // const express = require('express');
 import express from 'express';
+
+const app = express();
+
+app.get("/products", (req,res) => {
+    res.send("Server is ready");
+})
+
+app.listen(5000, () => {
+    console.log("Server started at http://localhost:5000");
+});
