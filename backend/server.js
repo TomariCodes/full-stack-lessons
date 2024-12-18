2222a64b6ad6e0bbea706865bb510dfadd4bd9f2
Tomari <tomari7878@gmail.com>
2024-12-17 19:28:52 -0600

    created new file and imported mongooseDB, express, and dotevnv.

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,20 @@
 // const express = require('express');
-import express from 'express';
+import express from "express";
+import dotenv from "dontenv";
+import { connectDB } from "./config/db.js";
+
+dotenv.config();
 
 const app = express();
 
 app.get("/products", (req,res) => {
     res.send("Server is ready");
-})
+});
+
+console.log(process.env.MONGO_URL);
 
 app.listen(5000, () => {
+    connectDB();
     console.log("Server started at http://localhost:5000");
 });
 
