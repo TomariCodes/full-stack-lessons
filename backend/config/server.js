0d48d36615f39beee9b7ad6a90d12c893e16e3b8
Tomari <tomari7878@gmail.com>
2024-12-21 19:35:04 -0600

    fixed dotenv import

--- backend/config/server.js
+++ backend/config/server.js
@@ -1,19 +1,18 @@
 // const express = require('express');
 import express from "express";
-import dotenv from "dontenv";
-import { connectDB } from "./config/db.js";
+import dotenv from "dotenv";
+import { connectDB } from "./db.js";
 
 dotenv.config();
 
 const app = express();
 
-app.get("/products", (req,res) => {
-    res.send("Server is ready");
+app.get("/products", (req, res) => {
+  res.send("Server is ready");
 });
 
 
 app.listen(5000, () => {
-    connectDB();
-    console.log("Server started at http://localhost:5000");
+  connectDB();
+  console.log("Server started at http://localhost:5000");
 });
-
