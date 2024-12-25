c69127c8ebcfe6d76034c3a98f2ce85fdc41aa7f
Tomari <tomari7878@gmail.com>
2024-12-24 23:30:33 -0600

    created try catch statement for getting a product

--- backend/config/server.js
+++ backend/config/server.js
@@ -8,7 +8,7 @@ dotenv.config();
 
 const app = express();
 
-app.post("/products", (req, res) => {
+app.post("/products", async (req, res) => {
   const product = req.body; // user will send this data 
 
   if(!product.name || !product.price || !product.image) {
@@ -25,6 +25,17 @@ app.post("/products", (req, res) => {
     res.status(500).json({success:false, message: "Server Error"});
   }
 });
+
+
+app.get("/products", async (req, res) => {
+  try {
+  const products = await Product.find();
+  res.status(200).json({success: true, data: products});
+  } catch (error) {
+    console.error("Error in fetch products", error.message);
+    res.status(500).json({success: false, message: "Server Error" });
+  }
+  });
 
 
 app.listen(5000, () => {
